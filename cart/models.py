e2cc63f196e3a6ee0c290dd78b852d8222363af9
grigorevskiy <grigorevskiiy@gmail.com>
2018-05-03 22:19:21 +0300

    Added add, delete, get_cart methods, and urls

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
+from product.models import Item
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, related_name='cart', on_delete=models.CASCADE)
+    item = models.ManyToManyField(Item)
+    last_change = models.DateTimeField(auto_now_add=True)
