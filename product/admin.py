83ea4052ff71d61ba8fea079dd7aac1342b41595
yurabysaha <yurabysaha@gmail.com>
2018-05-03 09:43:16 +0300

    Create Product's models and register it in admin

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import Item, ItemPhoto, Category
+
+
+class ItemPhotoInline(admin.TabularInline):
+    model = ItemPhoto
+    extra = 1
+
+
+class ItemAdmin(admin.ModelAdmin):
+    """Add photos to Item in Admin"""
+    inlines = [ItemPhotoInline]
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    name_en = models.CharField(max_length=255, verbose_name='Name on English')
+    name_de = models.CharField(max_length=255, verbose_name='Name on German')
+    description_en = models.TextField(verbose_name='Description on English')
+    description_de = models.TextField(verbose_name='Description on German')
+
+    def __str__(self):
+        return self.name_en
+
+
+class Item(models.Model):
+    class Meta:
+        verbose_name = "Item"
+        verbose_name_plural = "Items"
+
+    STATUS_CHOICES = (
+        (0, 'Not Published'),
+        (1, 'Published'),
+        (2, 'Sold'),
+    )
+
+    title_en = models.CharField(max_length=255, verbose_name='Name on English')
+    title_de = models.CharField(max_length=255, verbose_name='Name on German')
+    description_en = models.TextField(verbose_name='Description on English')
+    description_de = models.TextField(verbose_name='Description on German')
+    price = models.IntegerField(verbose_name='Price')
+    status = models.IntegerField(choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
+    author = models.CharField(max_length=255, verbose_name='Author name')
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title_en
+
+
+class ItemPhoto(models.Model):
+    item = models.ForeignKey(Item, related_name='photos', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='item-photos')
