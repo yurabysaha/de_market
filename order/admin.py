4341e01559eab6dc7f8477111641ddb629013b16
yurabysaha <yurabysaha@gmail.com>
2018-05-03 13:56:56 +0300

    Add Order model and functionality for order in admin side

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from order.models import Order
+
+
+class ItemInline(admin.StackedInline):
+    model = Order.items.through
+    extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """Add items to Order in Admin"""
+    inlines = [ItemInline]
+    list_filter = ('status',)
+    search_fields = ('pk',)
+    list_display = ('__str__', 'user', 'total', 'status')
+    exclude = ('items',)
+
+
+admin.site.register(Order, OrderAdmin)
