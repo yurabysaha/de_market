27d511f5a675ee1613b8ac8d503e514db49f7757
Seredyak1 <sanya.seredyak@gmail.com>
2018-05-04 21:22:00 +0300

    Change BASE_DIR to PROJECT_ROOT in settings

--- de_market/settings.py
+++ de_market/settings.py
@@ -64,7 +64,7 @@ PROJECT_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'templates', 'allauth')],
+        'DIRS': [os.path.join(PROJECT_ROOT, 'templates'), os.path.join(BASE_DIR, 'templates', 'allauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
