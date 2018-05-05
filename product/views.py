dd4206709c2747927b9565c0ad579a5b68afd433
yurabysaha <yurabysaha@gmail.com>
2018-05-05 21:43:35 +0300

    Fix similar html and exclude item from similar

--- product/views.py
+++ product/views.py
@@ -7,7 +7,7 @@ from product.models import Comment, Item, Category
 def open_detail(request, item_id):
         if request.method == "GET":
             item = get_object_or_404(Item, id=item_id)
-            similar_items = Item.objects.all()[:3]
+            similar_items = Item.objects.exclude(id=item_id)[:3]
             return render(request, 'product/detail.html', {'item': item, 'similar_items': similar_items})
 
 
