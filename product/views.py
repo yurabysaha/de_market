a1f818c45ffab52c71314716720ffe04527abb85
yurabysaha <yurabysaha@gmail.com>
2018-05-05 22:16:21 +0300

    Move card_items to Individual file.

--- product/views.py
+++ product/views.py
@@ -7,8 +7,8 @@ from product.models import Comment, Item, Category
 def open_detail(request, item_id):
         if request.method == "GET":
             item = get_object_or_404(Item, id=item_id)
-            similar_items = Item.objects.exclude(id=item_id)[:3]
-            return render(request, 'product/detail.html', {'item': item, 'similar_items': similar_items})
+            items = Item.objects.exclude(id=item_id)[:3]
+            return render(request, 'product/detail.html', {'item': item, 'items': items})
 
 
 def create_comment(request, item_id):
