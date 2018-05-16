09472fd11515085d53be8f8affc190449bac7e80
grigorevskiy <grigorevskiiy@gmail.com>
2018-05-16 14:11:21 +0300

    Added adding comments now user can add comment at details of item page.

--- product/urls.py
+++ product/urls.py
@@ -4,6 +4,6 @@ from product import views
 urlpatterns = [
     path('<int:item_id>/', views.open_detail, name='item-detail'),
     path('<int:item_id>/comment/<int:comment_id>/delete/', views.comment_delete, name = 'comment_delete'),
-    path('<int:item_id>/comment/create', views.create_comment, name='create_comment'),
+    path('<int:item_id>/', views.create_comment, name='create_comment'),
     path('<int:item_id>/comment/<int:comment_id>/update', views.update_comment, name = 'update_comment')
     ]
--- product/views.py
+++ product/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404, redirect
-
 from product.forms import CommentForm
 from product.models import Comment, Item, Category
 
@@ -8,23 +7,22 @@ def open_detail(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     items = Item.objects.exclude(id=item_id)[:3]
     comments = Comment.objects.all().filter(item=item)
-
-    return render(request, 'product/detail.html', {'item': item, 'items': items, 'comments':comments})
+    form = CommentForm()
+    if request.method == "POST":
+        create_comment(request, item_id)
+        return redirect('/item/{}'.format(item_id))
+    else:
+        return render(request, 'product/detail.html', {'item': item, 'items': items, 'comments':comments, 'form':form})
 
 
 def create_comment(request, item_id):
     if request.user.is_authenticated:
-        if request.method == "POST":
             form = CommentForm(request.POST)
             if form.is_valid():
                 form = form.save(commit=False)
                 form.user_id = request.user.id
                 form.item_id = item_id
                 form.save()
-                return redirect('/item/{}'.format(item_id))
-        else:
-            form = CommentForm()
-            return render(request, 'product/comment_create.html', {'form': form})
     else:
         return redirect('/')
 
