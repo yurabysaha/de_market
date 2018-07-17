b5051c3d5acab84493b8ac9142e1d74a24977b9c
yurabysaha <yurabysaha@gmail.com>
2018-07-17 14:18:47 +0300

    Improve querysets

--- product/views.py
+++ product/views.py
@@ -1,46 +1,40 @@
+from django.db.models import Prefetch
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
-from product.models import Category, Item
+from product.models import Category, Item, ItemPhoto
 from product.utils import handle_pagination
 from django.utils.translation import gettext as _
 
 
 def open_detail(request, item_id):
-    item = Item.objects.filter(id=item_id).first()
+    item = Item.objects.filter(id=item_id).prefetch_related(Prefetch('photos', queryset=ItemPhoto.objects.order_by('id'))).first()
     if not item:
         raise Http404
-    items = Item.objects.exclude(id=item_id).exclude(status=0)[:3]
+    items = Item.objects.exclude(id=item_id).exclude(status=0).prefetch_related(Prefetch('photos', queryset=ItemPhoto.objects.order_by('id')))[:3]
 
     return render(request, 'product/detail.html', {'item': item, 'items': items})
 
 
-def category_list(request):
-    categories = Category.objects.all()
-    return render(request, 'home.html', {'categories': categories})
-
-
 def category_detail(request, category_id):
     cat = get_object_or_404(Category, id=category_id)
-    categories = Category.objects.all()
+    categories = Category.objects.all().prefetch_related('sub_category')
 
     if cat.sub_category.count() > 0:
-        items = Item.objects.filter(category__parent=cat)
+        queryset = Item.objects.filter(category__parent=cat)
     else:
-        items = Item.objects.filter(category__id=category_id)
+        queryset = Item.objects.filter(category__id=category_id)
 
-    item = Item.objects.filter(category__id=category_id).first()
-    sub_categories = cat.sub_category.all()
+    items = queryset.prefetch_related(Prefetch('photos', queryset=ItemPhoto.objects.order_by('id')))
+
     return render(request, 'product/category_detail.html', {'categories': categories,
                                                             'items': handle_pagination(request, items, 12),
-                                                            'cat': cat,
-                                                            'item': item,
-                                                            'sub_categories': sub_categories})
+                                                            'cat': cat})
 
 
 def sales_items(request):
-    categories = Category.objects.all()
+    categories = Category.objects.all().prefetch_related('sub_category')
     cat = {'name': _('Discount')}
-    items = Item.objects.filter(sale_price__isnull=False)
+    items = Item.objects.filter(sale_price__isnull=False).prefetch_related(Prefetch('photos', queryset=ItemPhoto.objects.order_by('id')))
 
     return render(request, 'product/category_detail.html', {'categories': categories,
                                                             'cat': cat,
