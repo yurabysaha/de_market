091197c299fdf28c4201a0eb1b4315847896f5a9
yurabysaha <yurabysaha@gmail.com>
2018-07-20 15:16:15 +0300

    Refactoring

--- pages/views.py
+++ pages/views.py
@@ -8,22 +8,17 @@ from product.utils import handle_pagination
 def home(request):
     items = Item.objects.exclude(status=0).prefetch_related(Prefetch('photos', queryset=ItemPhoto.objects.order_by('id')))
     categories = Category.objects.all().prefetch_related('sub_category')
-    return render(request, 'home.html', {'items': handle_pagination(request, items, 12), 'categories': categories})
+    return render(request, 'pages/home.html', {'items': handle_pagination(request, items, 12), 'categories': categories})
 
 
 def about_us(request):
     categories = Category.objects.all()
-    return render(request, 'about_us.html', {'categories': categories})
+    return render(request, 'pages/about_us.html', {'categories': categories})
 
 
 def delivery(request):
     categories = Category.objects.all()
-    return render(request, 'delivery.html', {'categories': categories})
-
-
-def faq(request):
-    categories = Category.objects.all()
-    return render(request, 'faq.html', {'categories': categories})
+    return render(request, 'pages/delivery.html', {'categories': categories})
 
 
 def search(request):
@@ -43,9 +38,4 @@ def search(request):
     if max_price:
         queryset = queryset.filter(price__lte=max_price)
 
-    return render(request, 'search.html', {'items': handle_pagination(request, queryset, 12), 'categories': categories, 'selected': category_id})
-
-
-def site_map(request):
-    categories = Category.objects.order_by('name_en').all()
-    return render(request, 'site_map.html', {'categories': categories})
+    return render(request, 'pages/search.html', {'items': handle_pagination(request, queryset, 12), 'categories': categories, 'selected': category_id})
